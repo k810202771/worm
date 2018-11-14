793c2933442215030a1cbcf621d3443611a5feac
k810202771 <810202771@qq.com>
2018-11-14 19:02:07 +0800

    cc

---
+++ fw.php
@@ -0,0 +1,5 @@
+<?php
+    $txt = "platformType=1&productid=65179&objectId=5aab27ab2f301e00365c8971&productimg=https://img.piaoniu.com/poster/67a564d652cc610d35640112a9bd919822eb4dc5.jpg&field=2018年11月10日 周六 19:30&producttitle=话剧《海棠依旧》&visibility=true&count=1&amount=249&ticketCategoryId=undefined&ticketGroupId=4331512&deliverType=3&receiverName=康帅&phone=17600144545&postage=0&district=北京市北京市朝阳区&address=馨岛苑小区2#3_703&appId=10011&timeStamp=1541748753854&orderId=222991541748753854&sign=5147EEC3E07E4BED930B2B8EC58CACEF";
+    echo str_replace('platformType','a',$txt)
+
+?>
--- worm.js
+++ worm.js
@@ -1,54 +1,77 @@
 ﻿var Worm = function(options){
+	//初始化this
 	for(var i in options.data){
 		this[i] = options.data[i];
 	}
-	this.Initialization(options);
+	this.$Initialization(options);
 
 };
 Worm.prototype = {
 	_datas:[],
 	_fors:[],
-	first:false,
-	Transference:document.createElement("w-worm"),
+	$backstage:true,
+	$transference:document.createElement("w-worm"),
 	onload:function(){},
 	update:function(){},
-	Initialization:function(options){
+	//getkey
+	$getkey:function(key){
+		var bs = key.substr(0,1),keys = {$html:"innerHTML",$class:"className"};
+		switch(bs){
+		case "w":
+			key = key.substr(2,key.length);
+			break;
+		case ":":
+			key = key.substr(1,key.length);
+			break;
+		}
+		return keys["$" + key] || key;
+	},
+	//getinitialvalue
+	$getinitialvalue:function(str){
+		for(var z in this.$data){
+			str = str.replace(eval("/("+z+")(?=(?:[^\"]|\"[^\"]*\")*$)(?=(?:[^']|'[^']*')*$)/g"),"this." + z);
+		}
+		return str;
+	},
+	//insert For
+	$insertfor:function(el,value,key,name){
+		var expression = value.replace(/(^\s*)|(\s*$)/g,"").replace("  "," ").split(" ");
+		var parameter = expression[0].replace(" ","").split(",");
+		
+		var fordata = {
+			item:parameter[0],
+			index:parameter.length>1?parameter[1]:null,
+			type:expression[1]
+		};
+
+		var $transference = this.$transference.cloneNode();
+		this._fors.push({
+			initialvalue: this.$getinitialvalue(expression[2]),
+			fordata:fordata,
+			key:key,
+			el:$transference,
+			value:el,
+			lastvalue:null
+		});
+		var parentNode = el.parentNode;
+		el.removeAttribute(name);
+		parentNode.replaceChild($transference,el);
+	},
+	$Initialization:function(options){
 		this.$el = this.el || document.body;
 		this.$data = options.data || {};
 		delete this.el;
 		//删除多余的变量
 		delete options.data;
 		var that = this;
-		//getinitialvalue
-		function getinitialvalue(str){
-			console.log("开始：",str)
-			for(z in that.$data){
-				str = str.replace(eval("/("+z+")(?=(?:[^\"]|\"[^\"]*\")*$)(?=(?:[^\']|\'[^\']*\')*$)/g"),"this." + z);
-			}
-			console.log("结束：",str);
-			return str;
-		}
-		
-		//getkey
-		function getkey(key){
-			var bs = key.substr(0,1),keys = {$html:"innerHTML",$class:"className"};
-			switch(bs){
-			case "w":
-				key = key.substr(2,key.length);
-				break;
-			case ":":
-				key = key.substr(1,key.length);
-				break;
-			}
-			return keys["$" + key] || key;
-		}
+
 		//html初始化
 		//初始化动态数据
 		this.$el.innerHTML = this.$el.innerHTML.replace(/\{\{+(.+?)\}\}/g,"<w-worm>$1</w-worm>");
 		
 		var el = this.$("w-worm");
 		for(var i=0;i<el.length;i++){
-			el[i].innerHTML = getinitialvalue(el[i].innerHTML);
+			el[i].innerHTML = this.$getinitialvalue(el[i].innerHTML);
 
 			this._datas.push({
 				initialvalue:el[i].innerHTML,
@@ -65,31 +88,22 @@ Worm.prototype = {
 				for(var t=0;t<attrs.length;t++){
 					var bs = attrs[t].name.substr(0,2);
 					if(bs == "w-" || bs.substr(0,1) == ":"){
-						var key = getkey(attrs[t].name);
+						var key = this.$getkey(attrs[t].name);
 						switch(key){
 						case "for":
-							//var attrs[t].value.split(',');
-							this._fors.push({
-								initialvalue: attrs[t].value,
-								key:key,
-								el:element[i],
-								lastvalue:null
-							});
-							var parentNode = element[i].parentNode;
-							parentNode.insertBefore(element[i].cloneNode(),element[i]);
-							parentNode.removeChild(element[i]);
-							element[i].removeAttribute(attrs[t].name);
+							this.$insertfor(element[i],attrs[t].value,key,attrs[t].name);
 							break;
 						default:
-							attrs[t].value = getinitialvalue(attrs[t].value);
-							console.log(attrs[t].value);
-							this._datas.push({
-								initialvalue: attrs[t].value,
-								key:key,
-								el:element[i],
-								lastvalue:null
-							});
-							element[i].removeAttribute(attrs[t].name);
+							if(!attrs.getNamedItem('w-for') && !attrs.getNamedItem(':for')){
+								attrs[t].value = this.$getinitialvalue(attrs[t].value);
+								this._datas.push({
+									initialvalue: attrs[t].value,
+									key:key,
+									el:element[i],
+									lastvalue:null
+								});
+								element[i].removeAttribute(attrs[t].name);
+							}
 							break;
 						}
 
@@ -98,16 +112,55 @@ Worm.prototype = {
 			}
 		}	
 		//初始化完成后更新一次数据
-		this.refresh();
-		setInterval(function(){
-			that.refresh();
-		},10);
+		this.$refresh();
+
+		if(this.$backstage && window.addEventListener){
+			//窗口进入刷新
+			window.addEventListener("focus",function(){
+				that.$refresh();
+			})
+			//窗口退出暂停
+			window.addEventListener("blur",function(){
+				clearTimeout(that.$timer);
+			})
+		}
 
 		//初始化完成后
 		this.onload();
 	},
 	//更新数据
-	refresh:function(){
+	$refresh:function(){
+		var that = this;
+		//渲染for类型
+		for(var i=0;i<this._fors.length;i++){
+			this.$value = eval(this._fors[i].initialvalue);
+			//判断是否不执行
+			if(this._fors[i].lastvalue != this.$value){
+				this.update(this._fors[i]);
+				console.log(this._fors[i]);
+				
+				var elements = this._fors[i].el.children;
+				console.log(elements);
+				while (elements.length) {
+					console.log(elements);
+				}
+				if(elements.attributes && elements.attributes.length > 0){
+					var attrs = elements.attributes;  //得到所有属性
+					for(var t=0;t<attrs.length;t++){
+						var bs = attrs[t].name.substr(0,2);
+						if(bs == "w-" || bs.substr(0,1) == ":"){
+							console.log(attrs);
+						}
+					}
+				}
+				
+				eval("for(item " + this._fors[i].fordata.type + ' this.$value){this._fors[i].el.appendChild(this._fors[i].value.cloneNode(true))}')
+				//this.$insertfor(element[i],attrs[t].value,key,attrs[t].name);
+
+				this._fors[i].lastvalue = this.$value;
+			}
+		}
+		//渲染元素属性
 		for(var i=0;i<this._datas.length;i++){
 			this.$value = eval(this._datas[i].initialvalue);
 			//判断是否不执行
@@ -131,18 +184,23 @@ Worm.prototype = {
 			}
 		}
 		delete this.$value;
+
+		this.$timer = setTimeout(function(){
+			that.$refresh();
+		},10);
 	},
 	//获取元素
-	$:function(str){
+	$:function(str,ment){
+		ment = ment || document;
 		var name = str.substr(1,str.length);
 		switch(str.substr(0,1)){
 		case "#":
-			return document.getElementById(name);
+			return ment.getElementById(name);
 		case ".":
-			return document.getElementsByClassName(name);
+			return ment.getElementsByClassName(name);
 		default:
 			name = str.substr(0,str.length);
-			return document.getElementsByTagName(name);
+			return ment.getElementsByTagName(name);
 		}
 	}
 
